e37ca21c12839ab5a73e3019883ff78d989670f3
Štěpán Stenchlák <s.stenchlak@gmail.com>
2017-07-23 20:46:19 +0200

    Initial commit

---
+++ conf/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Default settings for the fksyearlistresults plugin
+ *
+ * @author Štěpán Stenchlák <stenchlak@fykos.cz>
+ */
+
+//$conf['fixme']    = 'FIXME';
---
+++ conf/metadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the fksyearlistresults plugin
+ *
+ * @author Štěpán Stenchlák <stenchlak@fykos.cz>
+ */
+
+
+//$meta['fixme'] = array('string');
+
